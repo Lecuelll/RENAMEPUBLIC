9cd341354cc3fe7d411aa6ba9b9f1043035f82c7
Lecuelll <114077160+Lecuelll@users.noreply.github.com>
2022-10-24 19:59:40 +0700

    Update haikal.js

--- haikal.js
+++ haikal.js
@@ -4324,7 +4324,7 @@ haikal.sendMessage(m.chat, { image: kalimage, caption: `◎ Owner : ${botname}
 ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
 *NOTE*
 🌷
-contact me Wa.me/13038350297
+contact me Wa.me/628951410332
 ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
 Base Script
 ©Haikal`, contextInfo:{"externalAdReply": {"title": `SC ZENU AIZAK`,"body": `Selamat ${salam} kak ${pushname}`,
